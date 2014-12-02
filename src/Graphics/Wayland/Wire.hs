fb64f240342b2dfb8e1eea034a2e59700062f464
Sivert Berg <code@trev.is>
2014-12-02 19:04:07 +0100

    Expose Wire stuff in Graphics.Wayland.Wire

--- src/Graphics/Wayland/Wire.hs
+++ src/Graphics/Wayland/Wire.hs
@@ -1,4 +1,20 @@
 module Graphics.Wayland.Wire
+    ( -- * Message
+      Message
+    -- * Socket
+    , Socket
+    , listen
+    , connect
+    , accept
+    , close
+    , send
+    , recv
+    -- * Encode / decode message
+    , toMessage
+    , fromMessage
+    )
 where
 
-
+import Graphics.Wayland.Wire.Encode
+import Graphics.Wayland.Wire.Message
+import Graphics.Wayland.Wire.Socket
