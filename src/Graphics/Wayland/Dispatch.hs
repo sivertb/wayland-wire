30690caccbe64e954b7a6b4126de156938e7991e
Sivert Berg <sivertb@stud.ntnu.no>
2014-12-25 17:38:21 +0100

    Rewrite Dispatch to allow code sharing
    Make it possible to define a single record that can be shared between
    the server and client code.

--- src/Graphics/Wayland/Dispatch.hs
+++ src/Graphics/Wayland/Dispatch.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 module Graphics.Wayland.Dispatch
-    ( Client
-    , Server
+    ( Side (..)
+    , RecordType (..)
     , Object (..)
     , Slots
     , Signals
@@ -12,23 +15,39 @@ module Graphics.Wayland.Dispatch
     , DispatchInterface (..)
     , objectFromNewId
     , newObject
-    , checkNew
+    , checkCons
     )
 where
 
 import Control.Monad
-import Control.Monad.IO.Class
+import Control.Monad.Error
 import Graphics.Wayland.Types
 import Graphics.Wayland.Wire.Message
 import Text.Printf
 
-data Client
-data Server
+data Side = Server | Client deriving (Eq, Show)
+data RecordType = Slot | Signal deriving (Eq, Show)
 
-newtype Object c i = Object { unObject :: ObjId }
+newtype Object (c :: Side) i = Object { unObject :: ObjId }
 
-data family Slots c i :: (* -> *)-> *
-data family Signals c i :: (* -> *) -> *
+type family Constructor (r :: RecordType) c i m where
+    Constructor Slot   c i m = (Object c i -> m (Slots c i m)) -> m (Object c i)
+    Constructor Signal c i m = Object c i -> m (Slots c i m)
+
+type family RetType (r :: RecordType) a where
+    RetType Signal a = a
+    RetType Slot   a = ()
+
+data family Requests (r :: RecordType) (c :: Side) i :: (* -> *) -> *
+data family Events   (r :: RecordType) (c :: Side) i :: (* -> *) -> *
+
+type family Slots (c :: Side) where
+    Slots Server = Requests Slot Server
+    Slots Client = Events   Slot Client
+
+type family Signals (c :: Side) where
+    Signals Client = Requests Signal Client
+    Signals Server = Events   Signal Server
 
 class Dispatch i c where
     dispatch :: MonadDispatch c m => Slots c i m -> Message -> m ()
@@ -54,16 +73,19 @@ newObject f = do
     f o >>= registerObject o
     return o
 
-checkNew :: (DispatchInterface i, Monad m)
-         => String
-         -> Int
-         -> ((Object c i -> m (Slots c i m)) -> m (Object c i))
-         -> m ()
-checkNew name ver cons =
-    unless (interfaceName iface == name && interfaceVersion iface >= ver) $
-        fail $ printf "Interface (%s, %i) does not match expected interface (%s, %i)\n"
+-- | Checks that the interface of a constructor matches the expected interface.
+checkCons :: forall e r c i m
+          .  (Error e, DispatchInterface i, MonadError e m)
+          => String                 -- ^ The expected interface name.
+          -> Int                    -- ^ The expected interface version.
+          -> Constructor r c i m    -- ^ The interface constructor.
+          -> m ()
+checkCons name ver cons =
+    unless (interfaceName iface == name && interfaceVersion iface >= ver)
+        . throwError . strMsg
+        $ printf "Interface (%s, %i) does not match expected interface (%s, %i)\n"
             (interfaceName iface) (interfaceVersion iface) name ver
     where
-        getIface :: ((Object c i -> m (Slots c i m)) -> m (Object c i)) -> i
+        getIface :: Constructor r c i m -> i
         getIface = undefined
         iface = getIface cons
