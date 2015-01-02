b50617d5f78d6dd3f1a186a4f0b09b36f3c9859c
Sivert Berg <sivertb@stud.ntnu.no>
2015-01-02 20:17:56 +0100

    Update Test to new scanner

--- src/Graphics/Wayland/Test.hs
+++ src/Graphics/Wayland/Test.hs
@@ -7,7 +7,5 @@ module Graphics.Wayland.Test
 where
 
 import Graphics.Wayland.TH
-import Language.Haskell.TH
 
-
-$(generateFromXml Server "/usr/share/wayland/wayland.xml")
+$(generateFromXml "/usr/share/wayland/wayland.xml")
