8c2c034a7d1db8193ca7bcbb8218489eac5d9304
Sivert Berg <code@trev.is>
2015-03-01 13:23:33 +0100

    Use cabal 1.20
    Cabal 1.22 did not play well with ghc-mod

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ cabal, cabalInstall, filterSource, hxt, networkMsg, utf8String, QuickCheck, mtl, free, dietSet, thLift }:
+{ cabal, cabalInstall_1_20_0_6, filterSource, hxt, networkMsg, utf8String, QuickCheck, mtl, free, dietSet, thLift }:
 
 cabal.mkDerivation
 ( self:
   { pname = "wayland-pure"
   ; version = "0.1.0"
   ; src = filterSource ./.
-  ; buildTools = [ cabalInstall ]
+  ; buildTools = [ cabalInstall_1_20_0_6 ]
   ; buildDepends = [ hxt networkMsg utf8String mtl free dietSet thLift ]
   ; testDepends = [ QuickCheck ]
   ; doCheck = true
