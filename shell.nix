1c7771360405cf518cdf30d6e62b879cd714988e
Sivert Berg <code@trev.is>
2016-06-22 21:35:59 +0200

    Make it possible to select compiler in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,19 @@
-{ haskellPackages ? (import <nixpkgs>{}).haskellPackages }:
+{ profiling ? false
+, ghc ? null
+}:
 let
+  orignalPackages =
+    if ghc == null
+    then (import <nixpkgs>{}).haskellPackages
+    else builtins.getAttr ghc ((import <nixpkgs>{}).haskell.packages);
+
+  haskellPackages =
+  if profiling
+  then orignalPackages.override {
+    overrides = self : super: { mkDerivation = drv : super.mkDerivation (drv // {enableLibraryProfiling = true;});
+    };
+  }
+  else orignalPackages;
   all = (import ../../nix-packages.nix) { haskellPackages = haskellPackages; isShell = true; };
 in
 all.wayland-wire.env
